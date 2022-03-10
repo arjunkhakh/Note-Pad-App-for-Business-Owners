709958fd87ea46927fa13757f7ec3b9d276dad34
Arjun Singh <arjunkhakh@gmail.com>
2022-03-10 16:29:48 +0000

    working on the get and post requests

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require("express");
+
+const app = express();
+
+const notesData = require('./Develop/db/db.json')
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server started on port http://localhost:${PORT}`);
+  });
+
+// Get Request to the notes page
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "notes.html"));
+  });
+
+// Get Request to the notes page
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+  });
+
+// Sets up the express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/api/terms', (req, res) => res.json(notesData));
+
+app.post("/api/notes", (req, res) => {
+    const newNote = {
+      title: req.body.title,
+      text: req.body.text,
+      id: uuid.v4(),
+    }
+});
