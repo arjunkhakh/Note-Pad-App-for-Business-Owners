a6562c3036cafc173a2a91bb877ab5d17d9f6149
Arjun Singh <arjunkhakh@gmail.com>
2022-03-11 11:27:26 +0000

    working on my routes and connecting it to work

--- server.js
+++ server.js
@@ -6,24 +6,27 @@ const notesData = require('./Develop/db/db.json')
 
 const PORT = process.env.PORT || 5000;
 
+const path = require('path')
+
 app.listen(PORT, () => {
     console.log(`Server started on port http://localhost:${PORT}`);
   });
 
 // Get Request to the notes page
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "notes.html"));
+    res.sendFile(path.join(__dirname, "Develop", "/public/notes.html"));
   });
 
 // Get Request to the notes page
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
+    res.sendFile(path.join(__dirname, "Develop", "/public/index.html"));
   });
 
 // Sets up the express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// API GET AND POST REQUESTS
 app.get('/api/terms', (req, res) => res.json(notesData));
 
 app.post("/api/notes", (req, res) => {
